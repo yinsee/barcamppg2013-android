70d073dd597ba552f29672aa3b2768b6ebbccbca
Tan Jun Rong <jr@favoritemedium.com>
2013-05-24 21:59:11 +0800

    Add in basic template with 3 tabs: Info, Friend, Update Profiles

--- src/com/barcamppenang2013/MainActivity.java
+++ src/com/barcamppenang2013/MainActivity.java
@@ -1,15 +1,26 @@
 package com.barcamppenang2013;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
+    private FragmentTabHost mTabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTabHost = (FragmentTabHost)findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+
+        mTabHost.addTab(mTabHost.newTabSpec("Info").setIndicator("Info"),
+        		InfoFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("Friends").setIndicator("Friends"),
+        		FriendFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("Update Profiles").setIndicator("Update Profiles"),
+        		UpdateProfileFragment.class, null);
     }
 
 
